586103b00691ea93c069a910828ea259061249b8
fahadkh14 <fahadkh9822@gmail.com>
2026-09-05 18:58:15 +0530

    fix: add ESLint 9 flat config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,21 +1,45 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2021: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  {
+    ignores: ["dist", "node_modules"],
   },
-  settings: { react: { version: 'detect' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react-refresh/only-export-components': 'warn',
+
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+    },
   },
-}
+];
